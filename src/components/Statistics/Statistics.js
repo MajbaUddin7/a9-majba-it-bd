3846603ce349810e886c38c55263eaaa64fa61e3
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-12 22:41:06 +0600

    Statistics file work complete

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
-// const Statistics = () => {
-const data = useLoaderData();
-// console.log(data.data);
-return (
-        // <div>
+const Statistics = () => {
+    const data = useLoaderData();
+    console.log(data.data);
+    return (
+        <div>
             <h2 className='text-5xl font-medium my-8'>Quiz Content Analytics</h2>
             <hr className='w-1/2 mx-auto my-7' />
             <ResponsiveContainer width="80%" height={300} className='mx-auto'>
