754b167e93939e52134d4f6d87506358fb9ba50d
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-12 20:50:54 +0600

    React router dom setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layouts/Main/Main';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import QuizDetail from './components/QuizDetails/QuizDetail';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Home></Home>
+        },
+        {
+          path: '/topics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: 'quiz/:quizId',
+          loader: async ({ params }) => {
+            return fetch(https://openapi.programming-hero.com/api/quiz/${params.quizId})},
+              element: <QuizDetail></QuizDetail>
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <div>Sorry.This page is not Available.</div>
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
