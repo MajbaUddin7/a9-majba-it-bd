930ace350bd317399d289b5899d1d9c655f56102
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-12 22:47:17 +0600

    Topic.js file complete

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
 
-// const Topics = () => {
-const quizes = useLoaderData();
-const allQuizes = quizes.data;
+const Topics = () => {
+    const quizes = useLoaderData();
+    const allQuizes = quizes.data;
 
-return (
-    <div className='md:px-20 px-7'>
-        <div className="quizes grid md:grid-cols-3 gap-12 my-8">
-            {
-                allQuizes.map(quiz => <Quiz
-                    key={quiz.id}
-                    quiz={quiz}
-                ></Quiz>)
-            }
+    return (
+        <div className='md:px-20 px-7'>
+            <div className="quizes grid md:grid-cols-3 gap-12 my-8">
+                {
+                    allQuizes.map(quiz => <Quiz
+                        key={quiz.id}
+                        quiz={quiz}
+                    ></Quiz>)
+                }
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Topics;
